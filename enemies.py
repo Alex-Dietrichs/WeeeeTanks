342594a02d07969373920d0566e55b5cf7fda5ce
Alex-Dietrichs <alex.j.dietrich@gmail.com>
2022-11-15 17:59:48 -0500

    level background

--- enemies.py
+++ enemies.py
@@ -1,11 +1,15 @@
 import objects
 
 class Enemy(objects.tank):
-    def __init__(self, x, y,  height, width, speed) -> None:
-        super().__init__(height, width, speed)
+    def __init__(self, x, y, speed) -> None:
+        super().__init__(x, y, speed)
         self.target = (x,y)
     def fire():
         pass
     
     def findPath():
         pass
+
+class tanEnemy(Enemy):
+    def __init__(self, x, y) -> None:
+        super().__init__(x, y,0)
--- main.py
+++ main.py
@@ -3,43 +3,47 @@ import time
 import objects
 import players
 import enemies
+import levels
 def appStarted(app): 
     app.enemyTanks = []
     app.enemyBullets = []
     app.playerBullets = []
-    app.currentLevel = 0
+    app.currentLayout = []
     app.levels = initLevels()
     app.currentLevel = 0
     app.missionLoading = False
-    app.totalLevels = 2
     app.lives = 3
+    app.wallSize = 50
+    app.background = app.loadImage('images\\background.png')
+    app.time0 = time.time()
 def timerFired(app):
     if(app.currentLevel > 0 and not app.missionLoading):
         doCollisions()
         doMove()
         if(len(app.enemyTanks) == 0):
-            completeLevel()
+            completeLevel(app)
+    if(time.time() - app.time0 > 2 and app.missionLoading):
+        startLevel(app)
 
 def keyPressed(app, event):
     pass
 
 def keyReleased(app, event):
-    app.messages.append('keyReleased: ' + event.key)
+    pass
 
 def mousePressed(app, event):
-    app.messages.append(f'mousePressed at {(event.x, event.y)}')
+    pass
 
 def mouseReleased(app, event):
-    app.messages.append(f'mouseReleased at {(event.x, event.y)}')
+    if(app.currentLevel == 0 and event.x > 350 and event.x < 750
+     and event.y > 300 and event.y < 500):
+        completeLevel(app)
 
 def mouseMoved(app, event):
-    app.messages.append(f'mouseMoved at {(event.x, event.y)}')
-
-def mouseDragged(app, event):
-    app.messages.append(f'mouseDragged at {(event.x, event.y)}')
+    pass
 
 def sizeChanged(app):
-    app.messages.append(f'sizeChanged to {(app.width, app.height)}')
+    app.setSize(880,640)
 
 def redrawAll(app, canvas):
     if app.missionLoading:
@@ -51,24 +55,47 @@ def redrawAll(app, canvas):
         drawObjects(app,canvas)
 
 def drawHomeScreen(app,canvas):
-    pass
+    canvas.create_rectangle(350,300,750,500,fill = 'grey')
+    canvas.create_text(550,400,text = 'Start', font = 'Arial 40 bold')
+
 def drawLevel(app,canvas):
-    levelData = app.levels[app.currentLevel][2]
+    levelData = app.levels[app.currentLevel-1][2]
+    canvas.create_image(500,400,image = ImageTk.PhotoImage(app.background))
+    for (row,col) in levelData:
+        drawWall(canvas,row-1,col-1)
+    drawOutsideWalls(app,canvas)
+
+def drawOutsideWalls(app,canvas):
+    for i in range(22):
+        drawWall(canvas,i,0)
+        drawWall(canvas,i,15)
+    for j in range(16):
+        drawWall(canvas,0,j)
+        drawWall(canvas,21,j)
+def drawWall(canvas,row,col):
+    cx,cy = cellToLocation((row,col))
+    canvas.create_rectangle(cx-20,cy-20,cx+20,cy+20,fill = 'tan4')
 
 def drawObjects(app,canvas):
     pass
-def drawMissionLoad():
+def drawMissionLoad(app,canvas):
     pass
+
 #level format is list of tuple (Player,EnemyList,WallTupleList)
 def initLevels():
-    levels = []
-
-    return levels
+    gameLevels = []
+    for i in range(levels.totalLevels):
+        tempPlayer = levels.playerList[i]
+        tempEnemies = levels.enemyList[i] 
+        tempLayout = toTupleList(levels.layoutList[i])
+        gameLevels.append((tempPlayer,tempEnemies,tempLayout))
+    return gameLevels
 
 def startLevel(app):
-    app.player = app.levels[app.currentLevel][0]
-    app.enemyTanks
-    pass
+    app.player = app.levels[app.currentLevel-1][0]
+    app.enemyTanks = app.levels[app.currentLevel-1][1]
+    app.currentLayout = app.levels[app.currentLevel-1][2]
+    app.missionLoading = False
 
 def doCollisions():
     pass
@@ -76,11 +103,33 @@ def doMove():
     pass
 def completeLevel(app):
     app.currentLevel += 1
-    if(app.currentLevel > app.totalLevels):
+    if(app.currentLevel > levels.totalLevels):
         winGame(app)
     app.missionLoading = True
     app.time0 = time.time()
+    print(app.time0)
+
 def winGame(app):
     pass
 
-runApp(width=1000, height=800)
+def toTupleList(level):
+    tupleList = []
+    for i in range(len(level)):
+        for j in range(len(level[0])):
+            if level[i][j] == 1:
+                tupleList.append((i,j))
+    return tupleList
+
+def cellToLocation(cell):
+    i,j = cell
+    x = 20 + 40*i
+    y = 20 + 40*j
+    return (x,y)
+
+def locationToCell(location):
+    x,y = location
+    i = (x-20)//22
+    j = (y-20)//16
+    return (i,j)
+
+runApp(width=880, height=640)
